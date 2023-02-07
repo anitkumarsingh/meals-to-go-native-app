d261f4d1468a5908e012531aede61ca4c8614c78
anitkumarsingh <anikumarsingh99@gmail.com>
2023-02-07 20:22:25 +0530

    flex layout

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+	return (
+		<SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+			<View style={{ padding: 16, backgroundColor: 'green' }}>
+				<Text>Search Bar</Text>
+			</View>
+			<View style={{ flex: 1, padding: 16, backgroundColor: 'blue' }}>
+				<Text>Search Items</Text>
+			</View>
+			<ExpoStatusBar style='auto' />
+		</SafeAreaView>
+	);
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
+
+export default App;
